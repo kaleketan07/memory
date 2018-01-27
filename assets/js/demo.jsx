ea3c8028b2a302761ced4c235f8e242252d93fdc
Ketan <kaleketan07@gmail.com>
2018-01-27 09:08:18 -0500

    update in assets demo.jsx updated

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -12,39 +12,17 @@ class Demo extends React.Component {
     this.state = { side: props.side };
   }
 
-  toggle(side) {
-    var side = +!this.state.side;
-    this.setState({side: side});
-  }
+  
 
   render() {
-    var toggle = this.toggle.bind(this);
     return (
       <div className="row">
-        <Side show={this.state.side == 0} toggle={toggle} />
-        <div className="col">
-          &nbsp;
-        </div>
-        <Side show={this.state.side == 1} toggle={toggle} />
+        I changed the button!
       </div>
     );
   }
 }
 
-function Side(params) {
-  if (params.show) {
-    return (
-      <div id="side-0" className="side col" onMouseOver={ () => params.toggle() }>
-        <Button onClick={ () => alert("cheater") }>Click Me</Button>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div id="side-0" className="side col">
-        &nbsp;
-      </div>
-    );
-  }
-}
+
+
 
